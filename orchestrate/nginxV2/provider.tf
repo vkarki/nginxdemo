ebe6f417aa98611fc8e470971dcce1102cf9fac9
vkarki <54143475+vkarki@users.noreply.github.com>
2020-09-20 01:31:28 +0530

    Update provider.tf

--- orchestrate/nginxV2/provider.tf
+++ orchestrate/nginxV2/provider.tf
@@ -8,5 +8,11 @@ provider "azurerm" {
 terraform {
   required_version = ">= 0.12.0"
   backend "azurerm" {
+    subscription_id      = "b0199e68-059a-47f0-a685-74588c223168"
+    tenant_id            = "105b2061-b669-4b31-92ac-24d304d195dc"
+    resource_group_name  = "terraformDemo-rg"
+    storage_account_name = "demotfbackend"
+    container_name       = "tfstate"
+    key                  = "nginxDev.tfstate"
   }
 }
